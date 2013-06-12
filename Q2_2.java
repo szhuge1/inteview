e798729312b42ba9194cf96e97108c74b2a13376
szhuge1 <love_conan@hotmail.com>
2013-06-12 14:51:00 +0900

    Create Q2_2.java

---
+++ Q2_2.java
@@ -0,0 +1,27 @@
+/* Question:
+ * Implement an algorithm to find the nth to last element of a singly linked list
+ */
+
+package linked_list;
+
+public class Q2_2 {
+
+
+  public static LinkedListNode nthToLast1(LinkedListNode head, int n){
+		if (head == null || n < 1) {
+			return null;
+		}
+		LinkedListNode p1 = head;
+		LinkedListNode p2 = head;
+		for (int j = 0; j < n - 1; ++j) { // skip n-1 steps ahead
+			if (p2 == null) {
+				return null; // not found since list size < n 
+			}
+			p2 = p2.next; 
+		}
+		while (p2.next != null) { p1 = p1.next;
+		p2 = p2.next; }
+		return p1;
+	}
+}
+
